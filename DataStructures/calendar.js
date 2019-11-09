59ff6e955afb0b5550c801da37562e1beadfd993
Girish-Br <girishpacchu1427@gmail.com>
2019-11-09 17:40:20 +0530

    fourth commit

---
+++ DataStructures/calendar.js
@@ -0,0 +1,56 @@
+var readline=require("readline-sync");
+var calendar = require('../utility/dataStructuresProgramsUtility');
+/*
+* it will prints the month
+*/
+var months = [
+    "",
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"
+];
+/*
+* days[i] = number of days in month i
+*/
+var days = [
+    0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+];
+try {
+    var month =readline.question("enter month:");
+    var year = readline.question("enter year:");
+    var newMonth = parseInt(month);
+    var newYear = parseInt(year);
+
+    if (newMonth < 1 || newMonth > 12) throw "Month value is Invalid , Please Enter a value in range 1-12"
+    if (newMonth == undefined || newYear == undefined) throw "No input found"
+    if (isNaN(newMonth) || isNaN(newYear)) throw "No input or String found , Please Enter a value in range 1-12";
+    if (newMonth % 1 != 0 || newYear % 1 != 0) throw "Number required , Floating value found"
+    /*
+    * check for leap year
+    */
+    if (newMonth == 2 && calendar.isLeapYear(newYear)) days[newMonth] = 29;
+    console.log("   " + months[newMonth] + " " + year);
+    console.log(" S  M Tu  W Th  F  S");
+    /*
+    * starting day
+    */
+    var day = calendar.day(newMonth, 1, newYear);
+    /*
+    * print the calendar
+    */
+    for (let i = 0; i < day; i++) {
+        console.log("   ");
+    }
+    for (var i = 1; i <= days[newMonth]; i++) {
+        console.log(" ", i);
+        if (i < 10){
+            console.log(" ");
+        }
+        if (((i + day) % 7 == 0) || (i == days[newMonth])) {
+            console.log(" ");
+        }
+    }
+} catch (err) {
+    console.log("Error: " + err);
+}
---
+++ DataStructures/weekDayQueueStack.js
@@ -0,0 +1,87 @@
+var readline=require("readline-sync");
+var calendar=require('../utility/dataStructuresProgramsUtility');
+var linkedQueue=require('../utility/dataStructuresProgramsUtility');
+var linkedStack=require('../utility/dataStructuresProgramsUtility');
+/*
+* creating calender function to create any new node with null values.
+*/
+function calender() { 
+    var stackLink1 = new linkedStack.StackedLink;
+    var stackLink2 = new linkedStack.StackedLink;
+
+    var linkedqueue = new linkedQueue.QueuedLinkedList;
+    var linkedqueue2 = new linkedQueue.QueuedLinkedList;
+    /*
+    * it will display the year
+    */
+    var months = [
+        "",
+        "January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"
+    ];
+    /*
+    * days[i] = number of days in month i
+    */
+    var days = [
+        0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    ];
+    /*
+    * it will display the day
+    */
+    var day = ["S", "M", "Tu", "W", "Th", "F", "S"];
+
+    try {
+        var month = readline.question("enter month:");
+        var year = readline.question("enter year:");
+
+        var newMonth = parseInt(month);
+        var newYear = parseInt(year);
+
+        if (newMonth < 1 || newMonth > 12) throw "Month value is Invalid , Please Enter value in range 1-12"
+        if (newMonth == undefined || newYear == undefined) throw "No input found"
+        if (isNaN(newMonth) || isNaN(newYear)) throw "No input or String found , Please Enter a valid input";
+        if (newMonth % 1 != 0 || newYear % 1 != 0) throw "Number required , Floating value found"
+        /* 
+        * starting day
+        */
+        var d = calendar.day(newMonth, 1, newYear);
+        /*
+        * check for leap year
+        */
+        if (newMonth == 2 && calendar.isLeapYear(newYear)) days[newMonth] = 29;
+
+        console.log("     " + months[newMonth] + " " + year);
+        /*
+        * print the calendar
+        */
+        for (let i = 0; i < day.length; i++) {
+            linkedqueue.enque(day[i]);
+        }
+        for (let i = 0; i < d; i++) {
+            linkedqueue2.enque(" ");
+        }
+        for (let i = 1; i <= days[newMonth]; i++) {
+            linkedqueue2.enque(i);
+        }
+        for (let i = 0; i < 7; i++) {
+            stackLink1.push(linkedqueue.deque());
+        }
+        stackLink1.displayDays();
+
+        for (let i = 0; i < d; i++) {
+            stackLink2.push(linkedqueue2.deque());
+        }
+        for (let i = 1; i <= days[newMonth]; i++) {
+            stackLink2.push(linkedqueue2.deque());
+        }
+        stackLink2.reverse();
+        console.log();
+        stackLink2.displayCalender(d);
+        console.log(" ");
+    }   catch (err) {
+            console.log("Error: " + err);
+        }
+}
+calender();

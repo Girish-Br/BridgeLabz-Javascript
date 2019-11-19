c9e2c9c65d7a475f97f7334fffd86fcaf2a7bb70
Girish-Br <girishpacchu1427@gmail.com>
2019-11-19 12:30:16 +0530

    [Girish B R] completed DataStructures programs

--- DataStructures/OrderedList/orderedList.js
+++ DataStructures/OrderedList/orderedList.js
@@ -1,10 +1,10 @@
 //built in library for read file
 const fs = require("fs"); 
 //import the file 
-var linkedList=require('../utility/dataStructuresProgramsUtility'); 
+var linkedList=require('../../utility/dataStructuresProgramsUtility'); 
 var readline = require("readline-sync");
 //read the data txt file
-var data = fs.readFileSync('./file3.txt'); 
+var data = fs.readFileSync('./orderedList'); 
 console.log("Numbers data read from file: \n\n" + data);
  //user input to search element
 searchNumber = readline.question("Enter a number to search:");
--- DataStructures/UnOrderedList/unOrderedList.js
+++ DataStructures/UnOrderedList/unOrderedList.js
@@ -1,11 +1,11 @@
 //built in library for reading file
 const fs=require("fs");
 //to import data from linkedList file
-var util=require('../utility/dataStructuresProgramsUtility');
+var util=require('../../utility/dataStructuresProgramsUtility');
 //to make user interact with script via console
 var readline=require("readline-sync");
 //to take data from file2 
-var data = fs.readFileSync('./file2.txt');
+var data = fs.readFileSync('./unorderedList.txt');
 console.log("String read from file: \n\n"+data);
 //convert data to string and split
 stringArray= data.toString().toLocaleLowerCase().split(' ');

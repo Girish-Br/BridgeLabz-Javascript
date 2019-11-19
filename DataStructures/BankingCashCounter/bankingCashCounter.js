0bd3340cb601a4417a4acda55da8ee0c36a7aebf
Girish-Br <56828522+Girish-Br@users.noreply.github.com>
2019-11-19 17:53:39 +0530

    [Girish B R] completed Data Structures programs

--- DataStructures/BankingCashCounter/bankingCashCounter.js
+++ DataStructures/BankingCashCounter/bankingCashCounter.js
@@ -1,4 +1,4 @@
-/***************************************************
+/****************************************************
 * @purpose : Create a Program which creates Banking Cash Counter where people come in to deposit Cash and withdraw Cash. 
 *           Have an input panel to add people to Queue to either deposit or withdraw money and dequeue the people. 
 *           Maintain the Cash Balance.
